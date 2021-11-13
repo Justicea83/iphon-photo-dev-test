2e800d6f146b6c37570d541cdc2ad77ed366248d
Justicea83 <justicea83@gmail.com>
2021-11-13 12:19:40 +0000

    database migrations setup complete

---
+++ database/migrations/2021_11_13_121307_create_badges_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBadgesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('badges', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('achievement_count'); // the bigger the achievement the count, the bigger the badge
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('badges');
+    }
+}
